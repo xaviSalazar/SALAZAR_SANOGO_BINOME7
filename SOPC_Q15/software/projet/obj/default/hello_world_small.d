30d4e92ea5d647bd6ecc9c2784ffb1f61786502f
xaviSalazar <xavicoel@gmail.com>
2020-11-06 15:24:15 +0100

    SOPC_compteur Quartus 15 modification

---
+++ SOPC_Q15/software/projet/obj/default/hello_world_small.d
@@ -0,0 +1,9 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../projet_bsp//HAL/inc/sys/alt_stdio.h ../projet_bsp/system.h \
+ ../projet_bsp/linker.h
+
+../projet_bsp//HAL/inc/sys/alt_stdio.h:
+
+../projet_bsp/system.h:
+
+../projet_bsp/linker.h:
